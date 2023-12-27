e821ac6567430c165360d7a6ab4916fccc503686
uzair-bmj <uzairbmj@gmail.com>
2023-12-27 12:10:30 +0500

    SZC

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import './App.css'
+import Hero from './Components/Hero'
 
 export default function App() {
   return (
     <>
     <Navbar/>
+    <Hero/>
 
     </>
   )
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Hero.css'
+
+export default function Hero() {
+  return (
+    <>
+    <div className="herosection">
+        <div className="herotxt">
+        <h1>Finding your Perfect <br /> Items</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, voluptatibus!</p>
+        <button>Shop Now</button>
+        </div>
+        <div className="heroimg">
+            <img src="/Images/Hero1.jpg" alt="" />
+        </div>
+    </div>
+    
+    
+    
+    </>
+  )
+}
